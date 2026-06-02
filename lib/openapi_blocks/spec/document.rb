3b4ebabd88cfba398ea464c4d83ea4cfc1e1f9b3
Caio Santos <caio.francelinosena@gmail.com>
2026-06-01 20:00:42 -0400

    feat: refactor Document build method to improve paths handling and add tag generation

--- lib/openapi_blocks/spec/document.rb
+++ lib/openapi_blocks/spec/document.rb
@@ -17,17 +17,30 @@ module OpenapiBlocks
 
         components[:securitySchemes] = security.to_h if security&.schemes&.any?
 
+        paths = Paths.new.build
+
         doc = {
           openapi:    config.openapi_version,
           info:       config.info.to_h,
           servers:    config.to_h[:servers],
-          paths:      Paths.new.build,
-          components: components
+          paths:      paths,
+          components: components,
+          tags:       build_tags_from_paths(paths)
         }
 
         doc[:security] = security.schemes.keys.map { |s| { s => [] } } if security&.schemes&.any?
 
         doc
+      end
+
+      private
+
+      def build_tags_from_paths(paths)
+        names = paths.values.flat_map do |operations|
+          operations.values.flat_map { |op| Array(op[:tags]) }
+        end
+
+        names.uniq.map { |n| { name: n } }
       end
     end
   end
